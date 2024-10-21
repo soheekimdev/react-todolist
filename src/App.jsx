e1203642ddfaba33b7d3d707d262c44ea3cbcfaa
soheekimdev <soheekim.dev@gmail.com>
2024-10-21 12:08:06 +0900

    feat: 컴포넌트 분리, 수정 버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useRef, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   const [todolist, setTodolist] = useState([{ id: Number(new Date()), content: '안녕하세요' }]);
+
+  return (
+    <>
+      <TodoInput setTodolist={setTodolist} />
+      <TodoList todolist={todolist} setTodolist={setTodolist} />
+    </>
+  );
+};
+
+const TodoInput = ({ setTodolist }) => {
   const inputRef = useRef(null);
 
   const addTodo = () => {
@@ -11,24 +21,54 @@ function App() {
     inputRef.current.value = '';
   };
 
-  const removeTodo = (todo) => {
-    setTodolist((prev) => prev.filter((el) => el.id !== todo.id));
-  };
-
   return (
     <>
       <input ref={inputRef} />
       <button onClick={addTodo}>추가</button>
-      <ul>
-        {todolist.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button onClick={() => removeTodo(todo)}>삭제</button>
-          </li>
-        ))}
-      </ul>
     </>
   );
-}
+};
+
+const TodoList = ({ todolist, setTodolist }) => {
+  return (
+    <ul>
+      {todolist.map((todo) => (
+        <Todo key={todo.id} todo={todo} setTodolist={setTodolist} />
+      ))}
+    </ul>
+  );
+};
+
+const Todo = ({ todo, setTodolist }) => {
+  const [todoInput, setTodoInput] = useState(todo.content);
+  const [isReadOnly, setIsReadOnly] = useState(true);
+
+  const editTodo = () => {
+    if (!isReadOnly) {
+      setTodolist((prev) => prev.map((item) => (item.id === todo.id ? { ...item, content: todoInput } : item)));
+    }
+    setIsReadOnly(!isReadOnly);
+  };
+
+  const removeTodo = () => {
+    setTodolist((prev) => prev.filter((el) => el.id !== todo.id));
+  };
+
+  return (
+    <li>
+      <input
+        value={todoInput}
+        onChange={(e) => {
+          setTodoInput(e.target.value);
+        }}
+        readOnly={isReadOnly}
+      />
+      <div>
+        <button onClick={editTodo}>{isReadOnly ? '수정' : '저장'}</button>
+        <button onClick={removeTodo}>삭제</button>
+      </div>
+    </li>
+  );
+};
 
 export default App;
